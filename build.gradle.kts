210c8eb6cc61108a031b8df75933d690161b6b79
Jefry Jacky <jefryjacky@gmail.com>
2020-09-16 09:31:56 +0700

    add gson dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.springframework.security:spring-security-crypto")
+	implementation ("com.google.code.gson:gson:2.8.6")
 	runtimeOnly("mysql:mysql-connector-java")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
