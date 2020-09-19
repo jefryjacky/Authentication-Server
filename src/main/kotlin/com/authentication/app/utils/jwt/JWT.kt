136704de6d0ca4002da6866eeac878ee12d2d975
Jefry Jacky <jefryjacky@gmail.com>
2020-09-19 22:55:01 +0700

    jwt secret key

--- src/main/kotlin/com/authentication/app/utils/jwt/JWT.kt
+++ src/main/kotlin/com/authentication/app/utils/jwt/JWT.kt
@@ -1,12 +1,17 @@
 package com.authentication.app.utils.jwt
 
 import com.google.gson.Gson
+import org.springframework.beans.factory.annotation.Value
 import java.util.*
 
 /**
  * Created by Jefry Jacky on 16/09/20.
  */
 abstract class JWT {
+
+    @Value("\${jwt.secret}")
+    protected val jwtSecret: String? = null
+
     private val encoder = Base64.getEncoder()
     protected val gson = Gson()
 
--- src/main/kotlin/com/authentication/app/utils/jwt/JWTHS256.kt
+++ src/main/kotlin/com/authentication/app/utils/jwt/JWTHS256.kt
@@ -7,12 +7,13 @@ import javax.crypto.spec.SecretKeySpec
  * Created by Jefry Jacky on 16/09/20.
  */
 internal class JWTHS256: JWT() {
+
     override fun createHeader(): String {
         return gson.toJson(JWTHeader(SIGNING_ALGO, JWT_TYPE))
     }
 
     override fun createSignature(encodedHeader: String, encodedPayload: String): ByteArray {
-        val secretKey = "".toByteArray()
+        val secretKey = jwtSecret!!.toByteArray()
         val mac = Mac.getInstance("HmacSHA256")
         val secretKeySpec = SecretKeySpec(secretKey, "HmacSHA256")
         mac.init(secretKeySpec)
