1e17b6e3d557b7e71e2360c1ca9748599834312b
Jefry Jacky <jefryjacky@gmail.com>
2024-03-30 13:29:16 +0700

    refactor: user table id

--- src/main/kotlin/com/authentication/app/repository/entity/UserDB.kt
+++ src/main/kotlin/com/authentication/app/repository/entity/UserDB.kt
@@ -1,5 +1,7 @@
 package com.authentication.app.repository.entity
 
+import org.hibernate.annotations.GenericGenerator
+import org.hibernate.annotations.Parameter
 import javax.persistence.*
 
 /**
@@ -9,7 +11,14 @@ import javax.persistence.*
 @Table(name = "user_table")
 data class UserDB (
         @Id
-        @GeneratedValue(strategy = GenerationType.AUTO)
+        @GeneratedValue(generator = "sequence-generator")
+        @GenericGenerator(name = "sequence-generator",
+                strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+                parameters = [
+                        Parameter(name = "sequence_name", value = "user_sequence"),
+                        Parameter(name = "initial_value", value = "1"),
+                        Parameter(name = "increment_size", value = "1")
+                ])
         val userId: Long,
         @Column(unique = true)
         val email: String,
