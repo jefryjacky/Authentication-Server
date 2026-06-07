c0bb03daae751abe9dd515df57e1833e120990dc
jefry <jefryjacky@gmail.com>
2026-06-07 15:28:51 +0700

    chores: update access token lifespan

--- src/main/kotlin/com/authentication/app/domain/Constant.kt
+++ src/main/kotlin/com/authentication/app/domain/Constant.kt
@@ -4,5 +4,5 @@ package com.authentication.app.domain
  * Created by Jefry Jacky on 13/09/20.
  */
 const val REFRESH_TOKEN_LENGTH = 30
-const val ACCESS_TOKEN_EXPIRED_DURATION = 1209600000L
+const val ACCESS_TOKEN_EXPIRED_DURATION = 900000L
 const val REFRESH_TOKEN_EXPIRED_DURATION = 7257600000L
